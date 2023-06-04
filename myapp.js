ef5f6a3e983b7ee412aaa2d1114a1fc38d70baaf
NdumisoMagunga <magunga55@gmail.com>
2023-06-04 16:34:08 +0200

    working with console log to display data on the browser console

---
+++ myapp.js
@@ -0,0 +1,13 @@
+console.log('hello world');
+
+console.log(200);
+
+console.log(true);
+console.log(null);
+
+var num = 10;
+
+console.log(num);
+
+console.log("string", 10, num, true, null, 10.33);
+
