6f94d5747f0b2f866107b9732d97bc0f4a0fa6bc
NdumisoMagunga <magunga55@gmail.com>
2023-06-04 14:38:46 +0200

    first js commit

---
+++ basics.js
@@ -0,0 +1,31 @@
+//operators are functions
+// function is a subrutine
+ 
+// order of precedence
+ 
+
+let mathsd;
+
+mathsd = 5 + 2 * 3;
+
+console.log(mathsd);
+
+// assignment operators
+
+var number  = 20;
+
+number = 30;
+
+number -+ 40;
+
+number += 70;
+
+number /= 2;
+
+number %= 3;
+
+var str = "Focus" + "force";
+
+//Polymorphism
+//An organism or Inorganic objecct or material which 
+//takes Various forms
